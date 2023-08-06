aedea08053923ecb257d6f3de7546ba3a0cef257
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-08-06 16:43:50 -0400

    Hello world, make it basic

--- C#/Overview/HelloWorld/Program.cs
+++ C#/Overview/HelloWorld/Program.cs
@@ -1,14 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string str = Console.ReadLine();
+Console.WriteLine("Why, hello there " + str);
 
