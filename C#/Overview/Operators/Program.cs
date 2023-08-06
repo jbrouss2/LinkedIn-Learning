733cfc1bc4ca58c8bb878e0fb8480a5fae0da5bb
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-08-06 17:15:19 -0400

    Operators section

--- C#/Overview/Operators/Program.cs
+++ C#/Overview/Operators/Program.cs
@@ -12,30 +12,43 @@ namespace Operators
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine((x/y) * x);
+            Console.WriteLine(a + b);
 
 
             // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
-
+            x++;
+            y--;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             // TODO: Operators can be shorthand: a = a + b
-
+            a += b;
+            Console.WriteLine(a);
 
             // TODO: Logical operators &&, ||
-            // Console.WriteLine("----- Logic Operators -----");
+            Console.WriteLine("----- Logic Operators -----");
+            Console.WriteLine(x > y && y >= 5);
+            Console.WriteLine(x > y || y >= 5);
 
-
+            Console.WriteLine("----- Null-coalescing Operators -----");
             // null-coalescing operators
-            // string str = null;
+            string str = null;
             // TODO: the ?? operator uses left operand if not null, or right one if it is
-
+            Console.WriteLine(str ?? "Unknown string");
             
             // TODO: the ??= operator assigns the right operand if the left one is null
             // it replaces the code:
             // if (variable is null) {
             //    variable = somevalue;
             // }
-
+            str ??= "New String";
+            Console.WriteLine(str);
+            str = "hello";
+            str ??= "New String";
+            Console.WriteLine(str ?? "Unknown string");
+            Console.WriteLine(str);
         }
     }
 }
