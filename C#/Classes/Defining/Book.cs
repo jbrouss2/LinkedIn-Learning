868638ea562eddec3d2bee8b16371c8904a285e2
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-08-12 11:37:24 -0400

    Defining Classes

--- C#/Classes/Defining/Book.cs
+++ C#/Classes/Defining/Book.cs
@@ -6,12 +6,22 @@ namespace Defining
     public class Book
     {
         // TODO: classes have member variables, or "fields" to hold data
-
+        string _name;
+        string _author;
+        int _pageCount;
 
         // TODO: classes have one or more constructors
-
+        public Book(string name, string author, int pages)
+        {
+            _name = name;
+            _author = author;
+            _pageCount = pages;
+        }
 
         // TODO: methods are used to operate on the class and data
-
+        public string GetDescription()
+        {
+            return $"{_name} by {_author}";
+        }
     }
 }
