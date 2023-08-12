9964d9a7fb20ee8b3962ad321a622dfe09eaafa1
Juliana Broussard <jbrouss2@vols.utk.edu>
2023-08-12 11:29:36 -0400

    Palindrome challenge

---
+++ C#/Functions/ProgChallenge/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text;
+
+var word = "";
+(bool b, int l) result;
+Console.WriteLine("Let's Begin:");
+
+while (word != "exit")
+{
+    word = Console.ReadLine();
+    if(word != "exit")
+    {
+        result = isPalidrome(word);
+        Console.WriteLine($"Palindrome: {result.b}, Length: {result.l}");
+    }
+}
+
+(bool, int) isPalidrome (string checkFor)
+{
+    string testStr;
+
+    testStr = checkFor.ToUpper();
+
+    var sb = new StringBuilder();
+    foreach (char c in testStr)
+    {
+        if(!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+        {
+            sb.Append(c);
+        }
+    }
+
+    testStr = sb.ToString();
+
+    int i=0, j=testStr.Length-1;
+
+    while(i <= j)
+    {
+        if(testStr[i] != testStr[j])
+        {
+            return(false, 0);
+        }
+        i++;
+        j--;
+    }
+    return(true, testStr.Length);
+}
